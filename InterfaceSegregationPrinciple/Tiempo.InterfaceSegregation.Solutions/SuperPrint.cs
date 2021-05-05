0cc28b8e8172b611fab9772943651ff06e5bf53f
blaxzoser <luiscol50@hotmail.com>
2021-05-05 00:20:17 -0500

    Added new sample Interface Agregation

---
+++ InterfaceSegregationPrinciple/Tiempo.InterfaceSegregation.Solutions/SuperPrint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Tiempo.InterfaceSegregation.Solutions
+{
+
+    public class Document { }
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Printer : IPrinter
+    {
+        public void Print(Document d)
+        {
+            // Do something
+        }
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            // Do something
+        }
+
+        public void Scan(Document d)
+        {
+            // Do something
+        }
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner
+    {
+
+    }
+
+    public class MultiFunctionDevice : IMultiFunctionDevice
+    {
+        private readonly IPrinter _printer;
+        private readonly IScanner _scanner;
+
+        public MultiFunctionDevice(IPrinter printer, IScanner scanner)
+        {
+            _printer = printer;
+            _scanner = scanner;
+        }
+        public void Print(Document d)
+        {
+            _printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            _scanner.Scan(d);
+        }
+    }
+}
---
+++ Tiempo.InterfaceSegregation.Violations/Print.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Tiempo.InterfaceSegregation.Violations
+{
+    public class Document { }
+
+    public interface IMachine
+    {
+        void Print(Document document);
+
+        void Fax(Document document);
+
+        void Scan(Document document);
+    }
+
+    public class MultiFuctionalPrinter : IMachine
+    {
+        public void Fax(Document document)
+        {
+            //Do something
+        }
+
+        public void Print(Document document)
+        {
+            //Do something
+        }
+
+        public void Scan(Document document)
+        {
+            //Do something
+        }
+    }
+
+
+    public class CompacPrinter : IMachine
+    {
+        public void Fax(Document document)
+        {
+            throw new Exception("It cant do it");
+        }
+
+        public void Print(Document document)
+        {
+            //Do something like old school
+        }
+
+        public void Scan(Document document)
+        {
+            throw new Exception("It cant do it");
+        }
+    }
+}
